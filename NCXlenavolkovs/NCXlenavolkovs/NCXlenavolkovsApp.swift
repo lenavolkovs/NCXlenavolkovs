7a851d13d1db2050acb9ff96f707e377a53ade15
lenavolkovs <58178482+lenavolkovs@users.noreply.github.com>
2024-03-26 14:14:34 +0100

    Creating the project

---
+++ NCXlenavolkovs/NCXlenavolkovs/NCXlenavolkovsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NCXlenavolkovsApp.swift
+//  NCXlenavolkovs
+//
+//  Created by Elena Volkova on 26/03/24.
+//
+
+import SwiftUI
+
+@main
+struct NCXlenavolkovsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
